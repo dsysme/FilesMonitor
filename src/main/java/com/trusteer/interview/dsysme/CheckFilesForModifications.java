dd926759a3c2ddc2d3d14de27a097d8748cd00c5
dsysme <sharon.shmorak@gmail.com>
2016-12-27 16:56:46 +0200

    IU-2016.3.1 <Sharon@Bartimaeus Overwrite remote C:\Users\Sharon\IdeaProjects\FilesMonitor to local

--- src/main/java/com/trusteer/interview/dsysme/CheckFilesForModifications.java
+++
@@ -1,7 +0,0 @@
-package com.trusteer.interview.dsysme;
-
-public class CheckFilesForModifications {
-    public static void main(String[] args) {
-        ConfigurationLoader.INSTANCE.load();
-    }
-}
